bdb0c9a0ee96aab514e732a6d5d1e5bb4d03815e
Matthew <18314318+xavierblue@users.noreply.github.com>
2021-08-14 14:08:47 -0400

    Cha cha cha changes

--- SalesTax/MainWindow.xaml.cs
+++ SalesTax/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using SalesTax.Interfaces;
 using System;
 using System.Linq;
 using System.Windows;
+using System.Windows.Media;
 
 namespace SalesTax
 {
@@ -20,6 +21,24 @@ namespace SalesTax
 		private void Window_Loaded(object sender, RoutedEventArgs e)
 		{
 			CartItemType.ItemsSource = Enum.GetValues(typeof(CartItemType)).Cast<CartItemType>();
+		}
+
+		private void CartItemType_GotFocus(object sender, RoutedEventArgs e)
+		{
+			CartItemType.BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF7EB4EA");
+		}
+
+		private void CartItemType_LostFocus(object sender, RoutedEventArgs e)
+		{
+			CartItemType.BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#4864b8");
+		}
+
+		private void CartItemType_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+		{
+			if (e.Key == System.Windows.Input.Key.Space)
+			{
+				CartItemType.IsDropDownOpen = true;
+			}
 		}
 	}
 }
